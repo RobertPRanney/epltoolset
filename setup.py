22a725253700db812487b118da8498b14145305d
Robert Ranney <ranneyr@b20264.local>
2018-07-13 13:46:06 -0600

    first commt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='epltoolset',
+      version='0.1',
+      description='Toolset to simplify Oralce ETL script creation',
+      url='http://github.com.robertpranney/epltoolste',
+      author='Robert Ranney',
+      author_email='robertpranney@gmail.com',
+      license='MIT',
+      packages=['epltoolset'],
+      zip_safe=False)
